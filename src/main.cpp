eef06cfc427169a000979aeb4cf258d4f44a1a2d
AHerda <adrianherda@gmail.com>
2024-10-03 02:30:44 +0200

    Pierwszy test

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,26 @@ typedef struct {
 } airspy_device_my;
 
 int main(int argc, char* argv[]) {
-    std::cout << "Hello, World!" << std::endl;
+    airspy_device_my device;
+    int result = airspy_open(&device.device);
+    if (result != 0) {
+        std::cerr << "Error opening device" << std::endl;
+        return 1;
+    }
+
+    result = airspy_board_partid_serialno_read(device.device, &device.serial);
+
+    if (result != 0) {
+        std::cerr << "Error reading serial number" << std::endl;
+        return 1;
+    }
+
+    std::cout << "Serial number: " << device.serial.serial_no[0] << std::endl;
+
+    result = airspy_close(device.device);
+    if (result != 0) {
+        std::cerr << "Error closing device" << std::endl;
+        return 1;
+    }
     return 0;
 }
