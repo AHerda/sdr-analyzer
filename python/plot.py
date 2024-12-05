0d9900f7a869525ad6e6a25bd02ae83139f17d6a
AHerda <adrianherda@gmail.com>
2024-12-05 22:38:58 +0100

    plots with logscale and normalized heatmaps

--- python/plot.py
+++ python/plot.py
@@ -22,7 +22,7 @@ def main():
         if re.search(".csv$", file):
             print(f"processing: {file}")
             if re.search("heatmap", file):
-                create_histogram(data_path + file)
+                create_heatmap(data_path + file)
             else:
                 process_file(data_path + file)
 
@@ -54,39 +54,35 @@ def process_file(filename):
 
 # Plot with and without outliers
 def create_plot(filename, data, title, subtitle, outliers_removed=False):
-    plt.subplots_adjust(hspace=0.5)
-    fig, axs = plt.subplots(3, 1, figsize=(12, 9))
+    fig, ax = plt.subplots(figsize=(12, 9))
 
     myFmt = mdates.DateFormatter('%H:%M\n%d-%m')
 
     metrics = ['min', 'max', 'avg']
     colors = ['blue', 'red', 'green']
     labels = ['Min', 'Max', 'Avg']
-    # plt.title(title, fontsize=16)
 
-    for i, (metric, color, label) in enumerate(zip(metrics, colors, labels)):
-        axs[i].xaxis.set_major_formatter(myFmt)
-        axs[i].plot(data['datetime'], data[metric], label=label, color=color, alpha=0.7, linewidth=0.9)
-        # axs[i].set_title(f"{label} Values", fontsize=14)
-        axs[i].set_ylabel('Values', fontsize=12)
-        axs[i].grid(True, linestyle='--', alpha=0.5)
-        axs[i].legend()
+    plt.yscale("log")
 
-    axs[-1].set_xlabel('Datetime', fontsize=12)
-    # axs[0].set_title(title, fontsize=18)
-    # plt.xticks()
+    for metric, color, label in zip(metrics, colors, labels):
+        ax.plot(data['datetime'], data[metric], label=label, color=color, alpha=0.7, linewidth=0.9)
 
-    # Add subtitle with frequency and duration
+    ax.xaxis.set_major_formatter(myFmt)
+    ax.set_ylabel('Values', fontsize=12)
+    ax.grid(True, linestyle='--', alpha=0.5)
+    ax.legend()
+    ax.set_xlabel('Datetime', fontsize=12)
+
     plt.suptitle(subtitle, fontsize=10)
 
     # File name adjustment based on outliers condition
     suffix = '_no_outliers' if outliers_removed else ''
 
     fig.tight_layout(pad=2.0)
-    plt.savefig(f'plots2/{filename}{suffix}.png')
+    plt.savefig(f'plots3/{filename}{suffix}.png')
     plt.close()
 
-def create_histogram(filename):
+def create_heatmap(filename):
     data = pd.read_csv(filename, delimiter=';')
     frequency = data['frequency'].iloc[0]
 
@@ -119,11 +115,14 @@ def create_histogram(filename):
             maxs[r, c] /= counters[r, c]
 
     for data_array, name in [(avgs, "avgs"), (mins, "mins"), (maxs, "maxs")]:
+        print(np.linalg.norm(data_array[~np.isnan(data_array)]))
+        data_array /= np.linalg.norm(data_array[~np.isnan(data_array)])
+
         plt.figure(figsize=(8, 5))
         # plt.title(f"{title}")
         plt.imshow(data_array, cmap='coolwarm')
         plt.colorbar()
-        plt.savefig(f'plots2/{filename[5:-4]}_{name}.png')
+        plt.savefig(f'plots3/{filename[5:-4]}_{name}.png')
 
 # Removing outliers for the second plot
 def remove_outliers(df, column):
